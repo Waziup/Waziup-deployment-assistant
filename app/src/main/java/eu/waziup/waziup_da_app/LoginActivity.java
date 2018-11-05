a5a33277c2865777bb1bc8740fd9c9fb9491a1d1
Kidus Tekeste <kidusmamuye@gmail.com>
2018-11-05 13:13:37 +0300

    Initial Commit and project setup

---
+++ app/src/main/java/eu/waziup/waziup_da_app/LoginActivity.java
@@ -0,0 +1,13 @@
+package eu.waziup.waziup_da_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
