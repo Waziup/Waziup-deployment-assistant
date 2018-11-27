7125b40d9b6f4e2e79950dc6c54e858197074631
Kidus Tekeste <kidusmamuye@gmail.com>
2018-11-27 03:03:23 +0300

    wrote the notification handling with firebase

--- app/src/main/java/eu/waziup/waziup_da_app/utils/AppConstants.java
+++ app/src/main/java/eu/waziup/waziup_da_app/utils/AppConstants.java
@@ -12,6 +12,8 @@ public final class AppConstants {
     public static final String DB_NAME = "mindorks_mvp.db";
     public static final String PREF_NAME = "mindorks_pref";
 
+    public static final String PREF_KEY_FCM_ACCESS_TOKEN_NAME = "PREF_KEY_FCM_ACCESS_TOKEN_NAME";
+
     public static final long NULL_INDEX = -1L;
 
     public static final String TIMESTAMP_FORMAT = "yyyyMMdd_HHmmss";
