6a78c43aa014b2acadbffb1d6d19764404a36d7e
Kidus Tekeste <kidusmamuye@gmail.com>
2019-01-27 16:59:25 +0300

    created instrumentation testing for MapFragment and NotificationFragment

---
+++ app/src/androidTest/java/eu/waziup/app/ui/map/MapFragmentTest.java
@@ -0,0 +1,12 @@
+package eu.waziup.app.ui.map;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class MapFragmentTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
---
+++ app/src/androidTest/java/eu/waziup/app/ui/notification/NotificationFragmentTest.java
@@ -0,0 +1,12 @@
+package eu.waziup.app.ui.notification;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class NotificationFragmentTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
