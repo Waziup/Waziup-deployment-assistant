477a201cf3275f7ae2d4c7d028ccf717981c6435
Kidus Tekeste <kidusmamuye@gmail.com>
2019-01-30 10:59:29 +0300

    created JUnit testing for MainPresenter class

---
+++ app/src/test/java/eu/waziup/app/ui/main/MainMvpPresenterTest.java
@@ -0,0 +1,25 @@
+package eu.waziup.app.ui.main;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainMvpPresenterTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void onLogOutClicked() {
+    }
+
+    @Test
+    public void onFabClicked() {
+    }
+
+    @Test
+    public void onSensorItemClicked() {
+    }
+}
