cbe0eb570b40a35b4ab0f605f37a41bb2cdc0960
Kidus Tekeste <kidusmamuye@gmail.com>
2019-03-12 11:55:45 +0300

    updated the .travis.yml file for android Continuous Integration - CI testing

--- app/src/main/java/eu/waziup/app/ui/login/LoginActivity.java
+++ app/src/main/java/eu/waziup/app/ui/login/LoginActivity.java
@@ -94,7 +94,7 @@ public class LoginActivity extends BaseActivity implements LoginMvpView {
 //                // If sign in fails, display a message to the user.
 //                Log.w(TAG, "createUserWithEmail:failure", task.getException());
 //                Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-//                        Toast.LENGTH_SHORT).show();
+//                        Toast.LENGTH_SHORT).show(
 //                updateUI(null);
 //            }
 //
