f04e1163140dde24fa7aa422975b5aa5d75b9c02
KidusMT <kidusmamuye@gmail.com>
2019-05-16 13:42:05 +0300

    authentication worked with custom chrome tab

--- app/src/main/java/eu/waziup/app/ui/login/LoginActivity.java
+++ app/src/main/java/eu/waziup/app/ui/login/LoginActivity.java
@@ -1,15 +1,16 @@
 package eu.waziup.app.ui.login;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.customtabs.CustomTabsIntent;
 
 import net.openid.appauth.AuthorizationRequest;
 import net.openid.appauth.AuthorizationService;
+import net.openid.appauth.AuthorizationServiceConfiguration;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicReference;
 
 import javax.inject.Inject;
@@ -18,27 +19,14 @@ import butterknife.ButterKnife;
 import eu.waziup.app.R;
 import eu.waziup.app.ui.base.BaseActivity;
 import eu.waziup.app.ui.main.MainActivity;
-import eu.waziup.app.utils.AuthStateManager;
-import eu.waziup.app.utils.Configuration;
 
 public class LoginActivity extends BaseActivity implements LoginMvpView {
 
     public static final String TAG = "LoginActivity";
-    private static final String EXTRA_FAILED = "failed";
-    private static final int RC_AUTH = 100;
-    private final AtomicReference<String> mClientId = new AtomicReference<>();
-    private final AtomicReference<AuthorizationRequest> mAuthRequest = new AtomicReference<>();
-    private final AtomicReference<CustomTabsIntent> mAuthIntent = new AtomicReference<>();
 
     @Inject
     LoginMvpPresenter<LoginMvpView> mPresenter;
-
-    private AuthorizationService mAuthService;
-    private AuthStateManager mAuthStateManager;
-    private Configuration mConfiguration;
-    private CountDownLatch mAuthIntentLatch = new CountDownLatch(1);
-    private ExecutorService mExecutor;
-    private boolean mUsePendingIntents;
+    private final AtomicReference<AuthorizationRequest> mAuthRequest = new AtomicReference<>();
 
     public static Intent getStartIntent(Context context) {
         return new Intent(context, LoginActivity.class);
@@ -50,10 +38,6 @@ public class LoginActivity extends BaseActivity implements LoginMvpView {
 
         getActivityComponent().inject(this);
 
-        //check the current user
-//        if (mAuth.getCurrentUser() != null)
-//            openSensorActivity();
-
         setContentView(R.layout.activity_login);
         setUnBinder(ButterKnife.bind(this));
 
@@ -61,74 +45,34 @@ public class LoginActivity extends BaseActivity implements LoginMvpView {
 
         setUp();
 
+        AuthorizationServiceConfiguration serviceConfiguration = new AuthorizationServiceConfiguration(
+                Uri.parse("https://keycloak.staging.waziup.io/auth/realms/waziup/protocol/openid-connect/auth") /* auth endpoint */,
+                Uri.parse("https://keycloak.staging.waziup.io/auth/realms/waziup/protocol/openid-connect/token") /* token endpoint */
+        );
+
+        String clientId = "dashboard";
+        Uri redirectUri = Uri.parse("net.openid.appauthdemo:/oauth2redirect");
+        AuthorizationRequest.Builder builder = new AuthorizationRequest.Builder(
+                serviceConfiguration,
+                clientId,
+                AuthorizationRequest.RESPONSE_TYPE_CODE,
+                redirectUri
+        );
+        builder.setScopes("openid email profile");
+        AuthorizationRequest request = builder.build();
+
+        AuthorizationService authorizationService = new AuthorizationService(this);
+
+        String action = "com.google.codelabs.appauth.HANDLE_AUTHORIZATION_RESPONSE";
+        Intent postAuthorizationIntent = new Intent(action);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, request.hashCode(), postAuthorizationIntent, 0);
+        authorizationService.performAuthorizationRequest(request, pendingIntent);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
     }
-
-//    -->> SIGN UP NEW USERS
-//    mAuth.createUserWithEmailAndPassword(email, password)
-//            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//        @Override
-//        public void onComplete(@NonNull Task<AuthResult> task) {
-//            if (task.isSuccessful()) {
-//                // Sign in success, update UI with the signed-in user's information
-//                Log.d(TAG, "createUserWithEmail:success");
-//                FirebaseUser user = mAuth.getCurrentUser();
-//                updateUI(user);
-//            } else {
-//                // If sign in fails, display a message to the user.
-//                Log.w(TAG, "createUserWithEmail:failure", task.getException());
-//                Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-//                        Toast.LENGTH_SHORT).show(
-//                updateUI(null);
-//            }
-//
-//            // ...
-//        }
-//    });
-
-
-//    -->> SIGN IN EXISTING USERS
-//    mAuth.signInWithEmailAndPassword(email, password)
-//            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//        @Override
-//        public void onComplete(@NonNull Task<AuthResult> task) {
-//            if (task.isSuccessful()) {
-//                // Sign in success, update UI with the signed-in user's information
-//                Log.d(TAG, "signInWithEmail:success");
-//                FirebaseUser user = mAuth.getCurrentUser();
-//                updateUI(user);
-//            } else {
-//                // If sign in fails, display a message to the user.
-//                Log.w(TAG, "signInWithEmail:failure", task.getException());
-//                Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-//                        Toast.LENGTH_SHORT).show();
-//                updateUI(null);
-//            }
-//
-//            // ...
-//        }
-//    });
-
-//    -->> ACCESS USER INFORMATION
-//    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//    if (user != null) {
-//        // Name, email address, and profile photo Url
-//        String name = user.getDisplayName();
-//        String email = user.getEmail();
-//        Uri photoUrl = user.getPhotoUrl();
-//
-//        // Check if user's email is verified
-//        boolean emailVerified = user.isEmailVerified();
-//
-//        // The user's ID, unique to the Firebase project. Do NOT use this value to
-//        // authenticate with your backend server, if you have one. Use
-//        // FirebaseUser.getIdToken() instead.
-//        String uid = user.getUid();
-//    }
 
     @Override
     protected void onResume() {
@@ -146,16 +90,10 @@ public class LoginActivity extends BaseActivity implements LoginMvpView {
         super.onDestroy();
     }
 
-
     @Override
     public void setUp() {
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-    }
 
     @Override
     public void openSensorActivity() {
