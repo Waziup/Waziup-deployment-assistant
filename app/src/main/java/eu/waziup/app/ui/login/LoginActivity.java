8dd0817ec96a8b9ecfe3884f196334a9d63fbf87
KidusMT <kidusmamuye@gmail.com>
2019-05-20 23:14:26 +0300

    removed fabric-crashlytics, appAuth, firebase: FCM and auth

--- app/src/main/java/eu/waziup/app/ui/login/LoginActivity.java
+++ app/src/main/java/eu/waziup/app/ui/login/LoginActivity.java
@@ -78,68 +78,6 @@ public class LoginActivity extends BaseActivity implements LoginMvpView {
 //        updateUI(currentUser);// TODO HAVE TO GET THE UPDATEUI() METHOD
     }
 
-//    -->> SIGN UP NEW USERS
-//    mAuth.createUserWithEmailAndPassword(email, password)
-//            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//        @Override
-//        public void onComplete(@NonNull Task<AuthResult> task) {
-//            if (task.isSuccessful()) {
-//                // Sign in success, update UI with the signed-in user's information
-//                Log.d(TAG, "createUserWithEmail:success");
-//                FirebaseUser user = mAuth.getCurrentUser();
-//                updateUI(user);
-//            } else {
-//                // If sign in fails, display a message to the user.
-//                Log.w(TAG, "createUserWithEmail:failure", task.getException());
-//                Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-//                        Toast.LENGTH_SHORT).show(
-//                updateUI(null);
-//            }
-//
-//            // ...
-//        }
-//    });
-
-
-//    -->> SIGN IN EXISTING USERS
-//    mAuth.signInWithEmailAndPassword(email, password)
-//            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//        @Override
-//        public void onComplete(@NonNull Task<AuthResult> task) {
-//            if (task.isSuccessful()) {
-//                // Sign in success, update UI with the signed-in user's information
-//                Log.d(TAG, "signInWithEmail:success");
-//                FirebaseUser user = mAuth.getCurrentUser();
-//                updateUI(user);
-//            } else {
-//                // If sign in fails, display a message to the user.
-//                Log.w(TAG, "signInWithEmail:failure", task.getException());
-//                Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-//                        Toast.LENGTH_SHORT).show();
-//                updateUI(null);
-//            }
-//
-//            // ...
-//        }
-//    });
-
-//    -->> ACCESS USER INFORMATION
-//    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//    if (user != null) {
-//        // Name, email address, and profile photo Url
-//        String name = user.getDisplayName();
-//        String email = user.getEmail();
-//        Uri photoUrl = user.getPhotoUrl();
-//
-//        // Check if user's email is verified
-//        boolean emailVerified = user.isEmailVerified();
-//
-//        // The user's ID, unique to the Firebase project. Do NOT use this value to
-//        // authenticate with your backend server, if you have one. Use
-//        // FirebaseUser.getIdToken() instead.
-//        String uid = user.getUid();
-//    }
-
     @Override
     protected void onResume() {
         super.onResume();
