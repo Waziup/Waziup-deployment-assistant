109dd97f67d5df6a6973db927e7240c044bff356
KidusMT <kidusmamuye@gmail.com>
2019-05-20 23:17:47 +0300

    removed methods and cleaned the login activity

--- app/src/main/java/eu/waziup/app/ui/login/LoginActivity.java
+++ app/src/main/java/eu/waziup/app/ui/login/LoginActivity.java
@@ -4,20 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 import javax.inject.Inject;
 
@@ -32,9 +19,6 @@ public class LoginActivity extends BaseActivity implements LoginMvpView {
 
     public static final String TAG = "LoginActivity";
     private final static int RC_SIGN_IN = 123;
-    FirebaseAuth mAuth;
-    FirebaseAuth.AuthStateListener mAuthListner;
-    GoogleSignInClient mGoogleSignInClient;
 
     @Inject
     LoginMvpPresenter<LoginMvpView> mPresenter;
@@ -52,14 +36,13 @@ public class LoginActivity extends BaseActivity implements LoginMvpView {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
 
         getActivityComponent().inject(this);
 
 
         //check the current user
-        if (mAuth.getCurrentUser() != null)
-            openSensorActivity();
+//        if (mAuth.getCurrentUser() != null)
+        openSensorActivity();
 
         setContentView(R.layout.activity_login);
         setUnBinder(ButterKnife.bind(this));
@@ -73,9 +56,6 @@ public class LoginActivity extends BaseActivity implements LoginMvpView {
     @Override
     protected void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-//        updateUI(currentUser);// TODO HAVE TO GET THE UPDATEUI() METHOD
     }
 
     @Override
@@ -88,12 +68,6 @@ public class LoginActivity extends BaseActivity implements LoginMvpView {
         super.onStop();
     }
 
-    @OnClick(R.id.btn_google_login)
-    void onGoogleClicked() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
     @Override
     protected void onDestroy() {
         mPresenter.onDetach();
@@ -103,67 +77,13 @@ public class LoginActivity extends BaseActivity implements LoginMvpView {
 
     @Override
     public void setUp() {
-        mAuth = FirebaseAuth.getInstance();
 
-        mAuthListner = firebaseAuth -> {
-            if (firebaseAuth.getCurrentUser() != null)
-                openSensorActivity();
-        };
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                if (account != null)
-                    firebaseAuthWithGoogle(account);
-                else Log.e(TAG, "GoogleSignInAccount account is NULL");
-            } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e);
-                // ...
-            }
-        }
-    }
 
-    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithCredential:success");
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        //
-                        if (user != null) {// the user has already logged in
-                            if (user.getDisplayName() != null && user.getEmail() != null && user.getPhotoUrl() != null) {
-                                mPresenter.onSaveUserInfo(user.getDisplayName(), user.getEmail(), user.getPhotoUrl().toString());
-                                Log.e("--->userInfo", user.getDisplayName());
-                            }
-                            openSensorActivity();
-                        }
-//                        mPresenter.updateUserInfo(user.getDisplayName(), );
-                        //updateUI(user);
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithCredential:failure", task.getException());
-                        Toast.makeText(this, "Aut Fail", Toast.LENGTH_SHORT).show();
-                        //updateUI(null);
-                    }
-                    // ...
-                });
     }
 
     @OnClick(R.id.btn_login)
